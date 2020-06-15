2488909cfb46c20691a160c49fef4914134e51ab
C.P.H. Peters <C.P.H.Peters@tilburguniversity.edu>
2020-06-15 14:35:55 +0200

    Update settings.py

--- code_experiment/settings.py
+++ code_experiment/settings.py
@@ -5,8 +5,45 @@ from os import environ
 # the session config can be accessed from methods in your apps as self.session.config,
 # e.g. self.session.config['participation_fee']
 
+mturk_hit_settings = {
+    'keywords': ['study', 'academic'],
+    'title': 'Answer Two Questions',
+    'description': 'This study is conducted anonymously. Answer two questions about your daily life.',
+    'frame_height': 500,
+    #'preview_template': 'global/MTurkPreview.html',
+    'template': 'global/mturk_template.html',
+    'minutes_allotted_per_assignment': 5,
+    'expiration_hours': 7*24, # 7 days
+    #'grant_qualification_id': 'YOUR_QUALIFICATION_ID_HERE',# to prevent retakes
+    # 'qualification_requirements': [
+    #     # No-retakers
+    #     {
+    #         'QualificationTypeId': "30LLG2NWCXJ09FSUO3LGVGEKYUS095",
+    #         'Comparator': "DoesNotExist",
+    #     },
+    #     # Only US
+    #     {
+    #         'QualificationTypeId': "00000000000000000071",
+    #         'Comparator': "EqualTo",
+    #         'LocaleValues': [{'Country': "US"}]
+    #     },
+    #     # At least x HITs approved
+    #     {
+    #         'QualificationTypeId': "00000000000000000040",
+    #         'Comparator': "GreaterThanOrEqualTo",
+    #         'IntegerValues': [500]
+    #     },
+    #     # At least x% of HITs approved
+    #     {
+    #         'QualificationTypeId': "000000000000000000L0",
+    #         'Comparator': "GreaterThanOrEqualTo",
+    #         'IntegerValues': [98]
+    #     },
+    #     ]
+}
+
 SESSION_CONFIG_DEFAULTS = dict(
-    real_world_currency_per_point=1.00, participation_fee=0.00, doc=""
+    real_world_currency_per_point=1.00, participation_fee=0.00, doc="", 'mturk_hit_settings': mturk_hit_settings
 )
 
 SESSION_CONFIGS = [
